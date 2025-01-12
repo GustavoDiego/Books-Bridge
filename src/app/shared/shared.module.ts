6cfd35fdbd3c58b82f6519576a185a3b2695a168
GustavoDiego <gustavodiego298@gmail.com>
2025-01-12 17:03:01 -0300

    ToolBar-Navigation V0.4
    Tirei algumas importações, visto que não uso mais o PrimeNg nesse componente.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarNavigationComponent } from './components/toolbar-navigation/toolbar-navigation.component';
-import { CardModule } from 'primeng/card';
-import { ToolbarModule } from 'primeng/toolbar';
-import { ButtonModule } from 'primeng/button';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { RouterModule } from '@angular/router';
 
