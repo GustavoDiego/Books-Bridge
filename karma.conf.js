c71bb4502f41a7f81310d92878a29064f6335bbe
GustavoDiego <gustavodiego298@gmail.com>
2025-01-17 15:32:01 -0300

    Teste V0.3
    nova abordagem para os testes automatizados

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,35 @@
-coverageReporter: {
-  type: 'html';
-  dir: 'coverage/';
-  subdir: '.'
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        random: true,
+        seed: '98765',
+        stopOnFailure: false,
+        timeoutInterval: 10000 
+      },
+      clearContext: false
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    restartOnFileChange: true
+  })
 }
